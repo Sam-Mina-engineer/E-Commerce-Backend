07339bb02bf6b49a3fe585b6a4d0cba1be22de45
Sam Mina <samandmissy2010@gmail.com>
2024-11-11 20:52:37 -0500

    routes api index

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,6 +3,8 @@ const categoryRoutes = require('./category-routes');
 const productRoutes = require('./product-routes');
 const tagRoutes = require('./tag-routes');
 
+//routes for each endpoint
+
 router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
 router.use('/tags', tagRoutes);
